0cbe2f90404596e86662c2da12cce872a5bd8f44
Shiwansh Das <learneransh24@gmail.com>
2026-07-15 21:26:58 +0530

    feat: integrate async bubble sort algorithm and DOM swapping

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const container = document.getElementById('visualizer-container');
 const generateBtn = document.getElementById('generate-btn');
 const sortBtn = document.getElementById('sort-btn');
 
+/* array generation */
 function generateArray(size = 30) {
     container.innerHTML = ''; 
     const array = [];
@@ -27,3 +28,27 @@ generateArray();
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+/*Bubble sort logic*/
+async function bubbleSort(){
+   const bars = document.querySelectorAll('.bar');
+
+   for(let i = 0 ;i < bars.length; ++i){
+    for(let j = 0; j < bars.length - i - 1; ++j){
+
+        const height1 = parseInt(bars[j].style.height);
+        const height2 = parseInt(bars[j+1].style.height);
+
+        if(height1 > height2){
+            bars[j].style.height = `${height2}px`;
+            bars[j+1].style.height = `${height1}px`;
+        }
+
+        await sleep(30);      //Pause execution of sorting for 30 ms for human eye to recognize the changes on the screen
+    }
+   }
+}
+
+sortBtn.addEventListener('click' , async () => {
+    await bubbleSort();
+});
